3a183c292bf0f98ad2936d3235ba155202cdfd85
Timur Aitkulov <aitku01@mail.ru>
2023-02-26 00:00:20 +0300

    first commit

---
+++ src/test/java/com/service/jewelry/JewelryApplicationTests.java
@@ -0,0 +1,16 @@
+package com.service.jewelry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+@SpringBootTest
+class JewelryApplicationTests {
+
+	@Test
+	void contextLoads() {
+		assertNull(null);
+	}
+
+}
