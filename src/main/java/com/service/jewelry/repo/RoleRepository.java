d5db71c79c7b15e3294ef5b0e18d6db8429eceb5
antonblkv <antonblkv@yandex.ru>
2023-03-16 11:54:56 +0300

    Need to add sql scripts and fix dependencies

---
+++ src/main/java/com/service/jewelry/repo/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.service.jewelry.repo;
+
+import com.service.jewelry.model.RoleEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<RoleEntity, Long> {
+}
---
+++ src/main/java/com/service/jewelry/repo/UserRepository.java
@@ -0,0 +1,8 @@
+package com.service.jewelry.repo;
+
+import com.service.jewelry.model.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    UserEntity findByUsername(String username);
+}
