e9cbef45bfce8875fddc4b77e170a1a14d785111
Timur Aitkulov <aitku01@mail.ru>
2023-03-17 04:39:10 +0300

    Checkpoint commit

---
+++ src/main/java/com/service/jewelry/model/Role.java
@@ -0,0 +1,27 @@
+package com.service.jewelry.model;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Entity
+@Table(name = "role")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+
+    public Role(String name) {
+        super();
+        this.name = name;
+    }
+
+}
--- src/main/java/com/service/jewelry/repo/UserRepository.java
+++ src/main/java/com/service/jewelry/repo/UserRepository.java
@@ -1,8 +1,11 @@
 package com.service.jewelry.repo;
 
+import com.service.jewelry.model.User;
 import com.service.jewelry.model.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface UserRepository extends JpaRepository<UserEntity, Long> {
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
     UserEntity findByUsername(String username);
 }
